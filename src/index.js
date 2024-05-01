95160570cccebd6efb44e0eb5f0fbbe309daa226
Megha N <n.megha82@gmail.com>
2024-05-01 13:33:26 -0700

    Initial Changes for S3 upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import UploadImageToS3WithNativeSdk from './UploadImageToS3WithNativeSdk';
 import reportWebVitals from './reportWebVitals';
+import CustomWebcam from './Webcam/CustomWebcam';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+ <UploadImageToS3WithNativeSdk/>
   </React.StrictMode>
 );
 
